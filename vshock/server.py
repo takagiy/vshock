6fbeff1b30065e0f5174a21d5d6b6bba41db2451
takagiy <takagiy.4dev@gmail.com>
2019-06-29 16:19:36 +0900

    add firendly messages and browser auto launching

--- vshock/server.py
+++ vshock/server.py
@@ -1,11 +1,13 @@
+import json
+import webbrowser
 from os import path
 from logging import getLogger, INFO
 from tornado import web
 from tornado.web import StaticFileHandler
 from tornado.websocket import WebSocketHandler
 from tornado.ioloop import IOLoop
-import json
 from .io import JoystickStream
+from multiprocessing import Process
 
 getLogger(__name__).setLevel(INFO)
 
@@ -37,4 +39,10 @@ def serve(port, stream_path, launch_browser):
                                    'default_filename': 'index.html'
                                })])
         app.listen(port)
-        IOLoop.instance().start()
+        listener = Process(target=lambda: IOLoop.current().start())
+        listener.start()
+        print("Controller UI is listening at http://localhost:%s." % port)
+        if launch_browser:
+            print("Opening the browser...")
+            webbrowser.open('http://localhost:%s' % port)
+        listener.join()
