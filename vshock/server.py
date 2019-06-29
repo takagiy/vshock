b9c20b2cb1e0c902f3260cfd729d78dbbe6751fd
takagiy <takagiy.4dev@gmail.com>
2019-06-29 12:41:07 +0900

    open jsstream once

--- vshock/server.py
+++ vshock/server.py
@@ -5,7 +5,7 @@ from tornado.web import StaticFileHandler
 from tornado.websocket import WebSocketHandler
 from tornado.ioloop import IOLoop
 import json
-from . import js_event
+from .io import JoystickStream
 
 getLogger(__name__).setLevel(INFO)
 
@@ -15,24 +15,26 @@ class WsServer(WebSocketHandler):
         self.js_stream = js_stream
         self.logger = getLogger(__name__)
 
-    def open(self):
+    def open(self, *args, **kwargs):
         self.logger.info('WebSocket connection has been established.')
 
     def on_close(self):
         self.logger.info('WebSocket connection has been broken up.')
 
     def on_message(self, message):
-        self.logger.info('Resieve a message: ' + repr(message))
+        self.logger.info('Resieve a message: %s', message)
         message = json.loads(message)
-        js_event.emit(self.js_stream, message)
+        self.js_stream.emit(message)
 
 
-def serve(port, js_stream, browser):
-    ui = path.join(path.dirname(__file__), 'assets/ui')
-    app = web.Application([(r"/input", WsServer, dict(js_stream=js_stream)),
-                           (r"/(.*)", StaticFileHandler, {
-                               'path': ui,
-                               'default_filename': 'index.html'
-                           })])
-    app.listen(port)
-    IOLoop.instance().start()
+def serve(port, stream_path, launch_browser):
+    with JoystickStream(stream_path) as js_stream:
+        ui = path.join(path.dirname(__file__), 'assets/ui')
+        app = web.Application([(r"/input", WsServer,
+                                dict(js_stream=js_stream)),
+                               (r"/(.*)", StaticFileHandler, {
+                                   'path': ui,
+                                   'default_filename': 'index.html'
+                               })])
+        app.listen(port)
+        IOLoop.instance().start()
